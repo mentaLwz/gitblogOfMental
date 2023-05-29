d90ad79f22e691888cee796d1c0fc044e0888c6d
mentaLwz <57254019+mentaLwz@users.noreply.github.com>
2023-05-29 23:43:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from github import Github
 from feedgen.feed import FeedGenerator
 from lxml.etree import CDATA
 
+
 MD_HEAD = """## Gitblog
 My personal blog using issues and GitHub Actions (随意转载，无需署名)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
